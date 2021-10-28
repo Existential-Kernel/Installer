d133399d9647543b70ff7d149d861db01de5ba6f
nonce <77142078+Existential-nonce@users.noreply.github.com>
2021-10-29 00:22:48 +0100

    Misc stuff

---
+++ scripts/github.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd ~/Documents/Repositories
+	git clone https://github.com/Existential-nonce/Fedora-Startup
+	git clone https://github.com/Existential-nonce/PassGen
+	git clone https://github.com/Existential-nonce/Guideline-webhooks
+	git clone https://github.com/Existential-nonce/Cursed-RPC
+	git clone https://github.com/Existential-nonce/Existential-nonce
+	git clone https://github.com/Existential-nonce/Existential-nonce
+cd ~/Documents/Cloned\ Repositories
+	git clone https://github.com/mtoyoda/sl.git
+	git clone https://github.com/jagannatharjun/qbt-theme.git
+	git clone https://github.com/jasonniebauer/Nmap-Cheatsheet.git
+	git clone https://github.com/cli/cli.git
+	mkdir tools 
+	cd tools
+	git clone https://github.com/Cryptonian007/Garuda.git
+	git clone https://github.com/sherlock-project/sherlock.git
+	cd sherlock
+	python3 -m pip install -r requirements.txt
+cd
